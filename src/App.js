9d9c0c62f15740e90c274da859ecb57ff7cee86b
Natalia Kulas <nataliakulas@gmail.com>
2018-06-27 19:42:41 +0200

    fetching dog pictures

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {pictures: []}
+  }
+
+  componentDidMount() {
+    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f30805949d59829de1f62b774103a3c2&text=dogs&per_page=100&page=1&format=json&nojsoncallback=1')
+      .then(resp => {
+        return resp.json();
+      })
+      .then(resp => {
+        let pictures = resp.photos.photo;
+
+        this.setState({pictures: pictures})
+      });
+  }
+
   render() {
     return (
       <div>
+        {this.state.pictures.length > 0 && this.state.pictures.map(picture => {
+          let path = 'https://farm' + picture.farm + '.staticflickr.com/' + picture.server + '/' + picture.id + '_' + picture.secret + '_s.jpg';
+          return <img key={picture.id} src={path} alt={'dogs'}/>
+        })}
       </div>
     );
   }
