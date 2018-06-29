6374d045e670ff3fd74b29cb3458e95f34365222
Natalia Kulas <nataliakulas@gmail.com>
2018-06-29 12:47:52 +0200

    navigation enhanced too

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-grid-system';
 
+import {Link} from 'react-router-dom';
+
+import * as routes from '../shared/routes';
+
 export default () =>
   <div className="navigation-wrapper">
     <Container>
       <Row>
         <Col>
           <div className="navigation">
-            <h1>Dog4Dog</h1>
+            <Link to={routes.DASHBOARD}><h1>Dog4Dog</h1></Link>
+            <div>
+              <Link to={routes.MAP}>
+                <div className="map"/>
+              </Link>
+            </div>
           </div>
         </Col>
       </Row>
