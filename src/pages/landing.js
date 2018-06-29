ffdc1b39de61d9230dea200c266fabc0f022f61b
Natalia Kulas <nataliakulas@gmail.com>
2018-06-29 11:58:55 +0200

    landing page enhanced

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -5,15 +5,15 @@ import {Link} from 'react-router-dom';
 import * as routes from '../shared/routes';
 
 
-const Landing = () =>
+export default () =>
   <Container>
     <Row>
-      <Col xs={12}>
-        <div className="full-page">
-          <Link to={routes.DASHBOARD} className="button">Search</Link>
+      <Col xs={10} md={6} lg={4} offset={{xs: 1, md: 3, lg: 4}}>
+        <div className="full-page landing">
+          <h1 className="blink">¡Hello!</h1>
+          <h2>This app allows you to view photos of&nbsp;dogs from flickr.</h2>
+          <Link to={routes.DASHBOARD} className="button">I want to see these dogs!</Link>
         </div>
       </Col>
     </Row>
   </Container>;
-
-export default Landing;
