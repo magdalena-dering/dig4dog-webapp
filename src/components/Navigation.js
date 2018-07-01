8fa1085efafd2f9065307a080064f422549ae2e6
Natalia Kulas <nataliakulas@gmail.com>
2018-07-01 19:05:40 +0200

    up arrow added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,20 +5,52 @@ import {Link} from 'react-router-dom';
 
 import * as routes from '../shared/routes';
 
-export default () =>
-  <div className="navigation-wrapper">
-    <Container>
-      <Row>
-        <Col>
-          <div className="navigation">
-            <Link to={routes.DASHBOARD}><h1>Dig4Dog</h1></Link>
-            <div>
-              <Link to={routes.MAP}>
-                <div className="map"/>
-              </Link>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </div>
+export class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {hidden: true}
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll, false);
+  }
+
+  onScroll = () => {
+    if (window.scrollY >= window.innerHeight) {
+      this.setState({hidden: false})
+    }
+    if (window.scrollY === 0) {
+      this.setState({hidden: true})
+    }
+  };
+
+  render() {
+    return (
+      <div className="navigation-wrapper">
+        <Container>
+          <Row>
+            <Col>
+              <div className="navigation">
+                <Link to={routes.DASHBOARD}><h1>Dig4Dog</h1></Link>
+                <div>
+                  <button type="button" onClick={() => window.scrollTo({top: 0, behavior: "smooth"})} className={this.state.hidden?"hidden":""}>
+                    {this.state.hidden ? <div style={{width: 30, height: 30}}/> : <div className="up"/>}
+                  </button>
+                  <Link to={routes.MAP}>
+                    <div className="map"/>
+                  </Link>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default Navigation;
